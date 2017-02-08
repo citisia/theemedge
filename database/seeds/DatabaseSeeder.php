c490d0521be459626e0d79a9679fba01c8b6ce47
Naved Deshmukh <naveddeshmukh@live.in>
2017-02-08 21:02:11 +0530

    Add a Database Seeder for Local environment

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        if(\App::environment('local'))
+            $this->call(LocalEnvironmentSeeder::class);
     }
 }
